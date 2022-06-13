c264d64b6e6c19c72083d18d72c615a430ccb999
박서연 <46805289+dkjefilsjl@users.noreply.github.com>
2022-06-13 21:28:28 +0900

    Update navercrolling.py
    오류처리 추가

--- navercrolling.py
+++ navercrolling.py
@@ -7,9 +7,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoAlertPresentException, NoSuchElementException, TimeoutException, ElementNotInteractableException,NoSuchWindowException, NoSuchFrameException
 import time
 
+#네이버 들어가기
 baseUrl = 'https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query='
 plusUrl = input('무엇을 검색할까요? : ')
-
+#검색
 url = baseUrl + quote_plus(plusUrl + " 등장인물")
 
 driver = webdriver.Chrome("C:/Users/User/chromedriver_win32/chromedriver.exe")
@@ -18,23 +19,25 @@ driver.get(url)
 html = driver.page_source
 soup = BeautifulSoup(html)
 list = []
-
-sl = soup.select('.title_box')
-
-for i in range(0, len(sl)):
-    aTag = sl[i].find('a')
-    list.append(aTag.string.split()[0])
-
-time.sleep(2)
-
-driver.find_element_by_xpath('//*[@id="main_pack"]/div[2]/div[1]/div[4]/div/div/ul/li[2]/a').send_keys(Keys.ENTER)
-
-html = driver.page_source
-soup = BeautifulSoup(html)
-
-sl = soup.select('.intro_box')[0].text.replace("'",'').replace('"','')
-sp = sl.split()
-for p in range(0, len(sp)):
+try:
+    #등장인물 찾기
+    sl = soup.select('.title_box')
+    #리스트에 저장
+    for i in range(0, len(sl)):
+        aTag = sl[i].find('a')
+        list.append(aTag.string.split()[0])
+    time.sleep(2)
+    #기본정보 클릭
+    driver.find_element_by_xpath('//*[@id="main_pack"]/div[2]/div[1]/div[4]/div/div/ul/li[2]/a').send_keys(Keys.ENTER)
+    html = driver.page_source
+    soup = BeautifulSoup(html)
+    #줄거리 가져오기
+    sl = soup.select('.intro_box')[0].text.replace("'",'').replace('"','')
+    sp = sl.split()
+    #리스트에 추가
+    for p in range(0, len(sp)):
         list.append(sp[p])
-
+except:
+        print("stop")
+#print(list)
 driver.close()
